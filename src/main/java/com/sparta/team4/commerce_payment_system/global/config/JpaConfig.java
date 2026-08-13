11a66bfba6639eba88bc0f793d0b80e00304443c
최정윤 <jes2ngyun@gmail.com>
2026-08-14 00:09:12 +0900

    feat: 회원 엔티티 및 JPA Auditing 설정

---
+++ src/main/java/com/sparta/team4/commerce_payment_system/global/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package com.sparta.team4.commerce_payment_system.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+}
