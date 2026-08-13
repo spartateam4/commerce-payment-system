999c604e5330ae61139cb9b217b7e7353c04fd01
Kang-SungHyun <rdevsh0104@gmail.com>
2026-08-13 15:30:35 +0900

    인프라 세팅

---
+++ src/main/java/com/sparta/team4/commerce_payment_system/CommercePaymentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.team4.commerce_payment_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommercePaymentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommercePaymentSystemApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/sparta/team4/commerce_payment_system/domain/cart/Cart.java
@@ -0,0 +1,5 @@
+package com.sparta.team4.commerce_payment_system.domain.cart;
+
+public class Cart {
+
+}
