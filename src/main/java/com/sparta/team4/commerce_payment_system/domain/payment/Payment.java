1ea09dd0698e9d28cecdfd43defc65058a9e3706
yeang976-art <yeang976@gmail.com>
2026-08-14 10:33:55 +0900

    feat: Payment 엔티티 초안 완성

--- src/main/java/com/sparta/team4/commerce_payment_system/domain/payment/Payment.java
+++ src/main/java/com/sparta/team4/commerce_payment_system/domain/payment/Payment.java
@@ -1,5 +1,52 @@
 package com.sparta.team4.commerce_payment_system.domain.payment;
 
-public class Payment {
+import com.sparta.team4.commerce_payment_system.domain.order.Order;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.LastModifiedDate;
 
+import java.time.LocalDateTime;
+
+@Entity
+//@Table(name = "payments")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Payment {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // Order가 아직 JPA 엔티티가 아님
+    //@OneToOne(fetch = FetchType.LAZY, optional = false)
+    //@JoinColumn(name = "order_id", nullable = false, unique = true)
+    //private Order order;
+
+    @Column(nullable = false)
+    private Integer amount;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 20)
+    private PaymentStatus status;
+
+    @LastModifiedDate
+    //@Column(nullable = false, name = "paid_at")
+    private LocalDateTime paidAt;
+
+    public Payment(Order order, Integer amount) {
+        //this.order = order;
+        this.amount = amount;
+        status = PaymentStatus.COMPLETED;
+        paidAt = LocalDateTime.now();
+    }
 }
---
+++ src/main/java/com/sparta/team4/commerce_payment_system/domain/payment/PaymentStatus.java
@@ -0,0 +1,5 @@
+package com.sparta.team4.commerce_payment_system.domain.payment;
+
+public enum PaymentStatus {
+    PENDING, COMPLETED, FAILED, CANCELLED
+}
