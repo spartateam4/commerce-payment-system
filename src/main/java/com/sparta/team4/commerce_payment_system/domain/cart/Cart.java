5bf7ae032a1a621526cec05de9ad49fdda1207f2
yun-ji98 <lyj77731@naver.com>
2026-08-14 12:05:53 +0900

    feat: cart entity생성

--- src/main/java/com/sparta/team4/commerce_payment_system/domain/cart/Cart.java
+++ src/main/java/com/sparta/team4/commerce_payment_system/domain/cart/Cart.java
@@ -1,5 +1,34 @@
 package com.sparta.team4.commerce_payment_system.domain.cart;
 
-public class Cart {
+import com.sparta.team4.commerce_payment_system.domain.member.entity.Member;
+import com.sparta.team4.commerce_payment_system.global.common.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "carts", uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"member_id"})
+})
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Cart extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    public Cart(Member member) {
+        this.member = member;
+    }
+
+    public Long getMemberId() {
+        return member.getId();
+    }
 
 }
--- src/main/java/com/sparta/team4/commerce_payment_system/domain/cart/CartItem.java
+++ src/main/java/com/sparta/team4/commerce_payment_system/domain/cart/CartItem.java
@@ -1,5 +1,64 @@
 package com.sparta.team4.commerce_payment_system.domain.cart;
 
-public class CartItem {
+import com.sparta.team4.commerce_payment_system.domain.product.Product;
+import com.sparta.team4.commerce_payment_system.global.common.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "cartsItems", uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"cart_id", "product_id"})
+})
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class CartItem extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "cart_id", nullable = false)
+    private Cart cart;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    @Column(name = "quantity", nullable = false)
+    private int quantity;
+
+    public CartItem(Cart cart, Product product, int quantity) {
+        this.cart = cart;
+        this.product = product;
+        if (quantity < 1) {
+            throw new IllegalArgumentException("수량은 1 이상이어야 합니다.");
+        }
+        this.quantity = quantity;
+    }
+
+    public Long getProductId() {
+        return product.getId();
+    }
+
+
+    // 동일 상품 중복 담기 시 기존 cartItem의 수량 누적 증가
+    public void addQuantity(int quantity) {
+        if (quantity < 1) {
+            throw new IllegalStateException("수량은 1 이상이어야 합니다.");
+        }
+        this.quantity += quantity;
+    }
+
+
+    // 수량 변경 시 사용자가 보낸 수량 값이 유효한 지 확인 후, 장바구니 항목의 수량을 그 값으로 바꿈
+    public void changeQuantity(int quantity) {
+        if (quantity < 1) {
+            throw new IllegalArgumentException("수량은 1 이상이어야 합니다.");
+        }
+        this.quantity = quantity;
+    }
 
 }
