7b3f6c3f7af52a47ad5ca6e331dcedb121a722b7
slvyblmn <slvyblmm@gmail.com>
2026-08-14 20:02:26 +0900

    feat: 상품 목록 응답 DTO 구현

---
+++ src/main/java/com/sparta/team4/commerce_payment_system/domain/product/dto/ProductListResponse.java
@@ -0,0 +1,29 @@
+package com.sparta.team4.commerce_payment_system.domain.product.dto;
+
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+public class ProductListResponse {
+
+    private List<ProductResponse> content;
+    private int page;
+    private int size;
+    private long totalElements;
+    private int totalPages;
+
+    public ProductListResponse(
+            List<ProductResponse> content,
+            int page,
+            int size,
+            long totalElements,
+            int totalPages
+    ) {
+        this.content = content;
+        this.page = page;
+        this.size = size;
+        this.totalElements = totalElements;
+        this.totalPages = totalPages;
+    }
+}
