54273962ca18712d746be87a81f9daa492c48dd9
slvyblmn <slvyblmm@gmail.com>
2026-08-18 16:00:48 +0900

    feat: 상품 등록 API 구현

--- src/main/java/com/sparta/team4/commerce_payment_system/domain/product/controller/ProductController.java
+++ src/main/java/com/sparta/team4/commerce_payment_system/domain/product/controller/ProductController.java
@@ -1,5 +1,6 @@
 package com.sparta.team4.commerce_payment_system.domain.product.controller;
 
+import com.sparta.team4.commerce_payment_system.domain.product.dto.ProductCreateRequest;
 import com.sparta.team4.commerce_payment_system.domain.product.dto.ProductResponse;
 import com.sparta.team4.commerce_payment_system.domain.product.service.ProductService;
 import lombok.RequiredArgsConstructor;
@@ -13,10 +14,21 @@ public class ProductController {
 
     private final ProductService productService;
 
+    // 상품 상세 조회
     @GetMapping("/{productId}")
     public ResponseEntity<ProductResponse> getProduct(@PathVariable Long productId) {
 
         ProductResponse response = productService.getProduct(productId);
+
+        return ResponseEntity.ok(response);
+    }
+
+    // 상품 등록
+    @PostMapping
+    public ResponseEntity<ProductResponse> createProduct(
+            @RequestBody ProductCreateRequest request) {
+
+        ProductResponse response = productService.createProduct(request);
 
         return ResponseEntity.ok(response);
     }
