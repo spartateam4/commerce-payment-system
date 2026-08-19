71cff5f16b9941a1d8d1ef4ea0c798081b120e99
Kang-SungHyun <rdevsh0104@gmail.com>
2026-08-19 16:47:08 +0900

    refactor:getOrderName 메서드 추가

--- src/main/java/com/sparta/team4/commerce_payment_system/domain/order/entity/Order.java
+++ src/main/java/com/sparta/team4/commerce_payment_system/domain/order/entity/Order.java
@@ -65,6 +65,13 @@ public class Order extends BaseEntity {
         changeStatus(OrderStatus.COMPLETED);
     }
 
+    public String getOrderName() {
+        if (orderItems.isEmpty()) return "주문";
+        String firstName = orderItems.get(0).getProductName();
+        if (orderItems.size() == 1) return firstName;
+        return firstName + " 외 " + (orderItems.size() - 1) + "건";
+    }
+
     // 상태 전이 규칙
     private void changeStatus(OrderStatus newStatus) {
         if (!this.status.canTransitTo(newStatus)) {
