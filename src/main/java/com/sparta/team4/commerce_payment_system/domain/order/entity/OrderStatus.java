aabf7775b31dc2ed12cd4d8b5b369e4e352aaf22
Kang-SungHyun <rdevsh0104@gmail.com>
2026-08-19 14:20:05 +0900

    refactor:상태 패턴 적용

--- src/main/java/com/sparta/team4/commerce_payment_system/domain/order/entity/OrderStatus.java
+++ src/main/java/com/sparta/team4/commerce_payment_system/domain/order/entity/OrderStatus.java
@@ -1,7 +1,24 @@
 package com.sparta.team4.commerce_payment_system.domain.order.entity;
 
 public enum OrderStatus {
-    PAYMENT_PENDING, // 결제 대기
-    COMPLETED,       // 주문 완료 (결제 성공)
-    CANCELLED        // 주문 취소
+    PAYMENT_PENDING {
+        @Override
+        public boolean canTransitTo(OrderStatus target) {
+            return target == COMPLETED || target == CANCELLED;
+        }
+    },
+    COMPLETED {
+        @Override
+        public boolean canTransitTo(OrderStatus target) {
+            return target == CANCELLED;
+        }
+    },
+    CANCELLED {
+        @Override
+        public boolean canTransitTo(OrderStatus target) {
+            return false;
+        }
+    };
+
+    public abstract boolean canTransitTo(OrderStatus target);
 }
