0509a4f0108761df44e074dcf0acfae47b774b67
yeang976-art <yeang976@gmail.com>
2026-08-20 17:13:18 +0900

    fix: DTO 수정

--- src/main/java/com/sparta/team4/commerce_payment_system/domain/payment/dto/CreatePaymentResponse.java
+++ src/main/java/com/sparta/team4/commerce_payment_system/domain/payment/dto/CreatePaymentResponse.java
@@ -1,20 +1,31 @@
 package com.sparta.team4.commerce_payment_system.domain.payment.dto;
 
-import com.sparta.team4.commerce_payment_system.domain.order.entity.OrderItem;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.sparta.team4.commerce_payment_system.domain.order.entity.OrderStatus;
 import com.sparta.team4.commerce_payment_system.domain.payment.entity.Payment;
 import com.sparta.team4.commerce_payment_system.domain.payment.entity.PaymentStatus;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
-public record CreatePaymentResponse(Long paymentId, Long orderId, Integer stock, Integer amount,
-                                    PaymentStatus paymentStatus, OrderStatus orderStatus, LocalDateTime paidAt) {
+public record CreatePaymentResponse(Long paymentId, Long orderId,
+                                    List<ProductStock> stock,
+                                    Integer amount,
+                                    @JsonProperty("pay_status") PaymentStatus paymentStatus,
+                                    @JsonProperty("order_status") OrderStatus orderStatus,
+                                    @JsonProperty("paid_at") LocalDateTime paidAt) {
     public static CreatePaymentResponse from(Payment payment) {
 
-        OrderItem orderItem = payment.getOrder().getOrderItems().get(0);
+        List<ProductStock> stocks = payment.getOrder().getOrderItems().stream()
+                .map(item -> new ProductStock(
+                        item.getProduct().getId(),
+                        item.getProduct().getStockQuantity()))
+                .toList();
 
         return new CreatePaymentResponse(payment.getId(), payment.getOrder().getId(),
-                orderItem.getProduct().getStockQuantity(), payment.getAmount(), payment.getStatus(),
+                stocks, payment.getAmount(), payment.getStatus(),
                 payment.getOrder().getStatus(), payment.getPaidAt());
     }
+
+    public record ProductStock(Long productId, Integer stock) {}
 }
--- src/main/java/com/sparta/team4/commerce_payment_system/domain/payment/dto/GetPaymentResponse.java
+++ src/main/java/com/sparta/team4/commerce_payment_system/domain/payment/dto/GetPaymentResponse.java
@@ -1,11 +1,13 @@
 package com.sparta.team4.commerce_payment_system.domain.payment.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.sparta.team4.commerce_payment_system.domain.payment.entity.Payment;
 import com.sparta.team4.commerce_payment_system.domain.payment.entity.PaymentStatus;
 
 import java.time.LocalDateTime;
 
-public record GetPaymentResponse(Integer amount, PaymentStatus status, LocalDateTime paidAt) {
+public record GetPaymentResponse(Integer amount, PaymentStatus status,
+                                 @JsonProperty("paid_at") LocalDateTime paidAt) {
     public static GetPaymentResponse from(Payment payment) {
         return new GetPaymentResponse(payment.getAmount(), payment.getStatus(), payment.getPaidAt());
     }
